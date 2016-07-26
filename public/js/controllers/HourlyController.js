7bbec148934b03ca2e8476a880bea3c4562afac5
Lahari <lahu.manchik@ufl.edu>
2016-07-26 19:11:04 -0400

    finished hourly view

--- public/js/controllers/HourlyController.js
+++ public/js/controllers/HourlyController.js
@@ -6,12 +6,16 @@
 
   function HourlyController($scope, WeatherService){
     $scope.hourlyData = WeatherService.weather;
-
+    $scope.summaryLookup = {
+      'Drizzle': 'There will be drizzling in your area',
+      'Partly Cloudy': 'There will be some clouds around your area',
+      'Light Rain': 'There will be some light rain in your area'
+    };
     $scope.$watch(function(){
       return WeatherService.weather;
     }, function(value){
       $scope.hourlyData = value;
-      
+
     });
   }
 })();
