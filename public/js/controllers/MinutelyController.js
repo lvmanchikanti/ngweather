396866816c36851f651b14dc067e03a536387769
Lahari <lahu.manchik@ufl.edu>
2016-07-26 20:40:46 -0400

    as of now finished minutelyView

--- public/js/controllers/MinutelyController.js
+++ public/js/controllers/MinutelyController.js
@@ -2,9 +2,16 @@
   angular.module('ngweather')
           .controller('MinutelyController', MinutelyController);
 
-  MinutelyController.$inject = [];
+  MinutelyController.$inject = ['$scope', 'WeatherService'];
 
-  function MinutelyController(){
+  function MinutelyController($scope, WeatherService){
+      $scope.minutelyData = WeatherService.weather;
 
+      $scope.$watch(function(){
+        return WeatherService.weather;
+      }, function(value){
+        $scope.minutelyData = value;
+
+      });
   }
 })();
